a9cce133f4eb3aca9a3ff2b140886cda1545bf65
maxherrera1964 <max.herrera@icloud.com>
2017-02-22 02:18:50 -0500

    Ran the code, converted to csv, changed column names to match program, generated plots form program

--- data/Plots/RollingSales_StatenIsland.R
+++ data/Plots/RollingSales_StatenIsland.R
@@ -6,12 +6,13 @@
 #install the gdata and plyr packages and load in to R.
 library(plyr)
 library(gdata)
-setwd("C:/MSDS 6306-SPRING2017/401/Live session 06")
 
+setwd("~/Documents/GitHub_Repositories/StatenIslandProject/data/")
 
 
 # So, save the file as a csv and use read.csv instead
-bk <- read.csv("rollingsales_brooklyn.csv",skip=4,header=TRUE)
+bk <- read.csv("rollingsales_statenisland.csv",sep=",",header=TRUE,quote = "\"")
+
 
 ## Check the data
 head(bk)
@@ -24,7 +25,8 @@ str(bk) # Very handy function!
 ## start with digits. We use the gsub command to replace them with a blank space.
 # We create a new variable that is a "clean' version of sale.price.
 # And sale.price.n is numeric, not a factor.
-bk$SALE.PRICE.N <- as.numeric(gsub("[^[:digit:]]","", bk$SALE.PRICE))
+
+bk$SALE.PRICE.N <- as.numeric(gsub("[^[:digit:]]","", bk$sale.price))
 count(is.na(bk$SALE.PRICE.N))
 
 names(bk) <- tolower(names(bk)) # make all variable names lower case
@@ -42,17 +44,17 @@ detach(bk)
 ## keep only the actual sales
 
 bk.sale <- bk[bk$sale.price.n!=0,]
-plot(bk.sale$gross.sqft,bk.sale$sale.price.n)
-plot(log10(bk.sale$gross.sqft),log10(bk.sale$sale.price.n))
+plot(bk.sale$gross.sqft,bk.sale$sale.price.n,main="All Sales NE 0",xlab="GROSS SQ FT",ylab="SALE PRICE")
+plot(log10(bk.sale$gross.sqft),log10(bk.sale$sale.price.n),main="All Sales NE 0",xlab="LOG 10 GROSS SQ FT",ylab="LOG 10 SALE PRICE")
 
 ## for now, let's look at 1-, 2-, and 3-family homes
 bk.homes <- bk.sale[which(grepl("FAMILY",bk.sale$building.class.category)),]
 dim(bk.homes)
-plot(log10(bk.homes$gross.sqft),log10(bk.homes$sale.price.n))
+plot(log10(bk.homes$gross.sqft),log10(bk.homes$sale.price.n),main="1-3 Family Home",xlab="GROSS SQ FT",ylab="SALE PRICE")
 summary(bk.homes[which(bk.homes$sale.price.n<100000),])
 
 
 ## remove outliers that seem like they weren't actual sales
 bk.homes$outliers <- (log10(bk.homes$sale.price.n) <=5) + 0
 bk.homes <- bk.homes[which(bk.homes$outliers==0),]
-plot(log10(bk.homes$gross.sqft),log10(bk.homes$sale.price.n))
+plot(log10(bk.homes$gross.sqft),log10(bk.homes$sale.price.n),main="HOME SALES NO OUTLIERS",xlab="GROSS SQ FT",ylab="SALE PRICE")
